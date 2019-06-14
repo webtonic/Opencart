ac11a3cfd47c505133078062cdff17f866246252
Ntiyiso Rikhotso <ntiyiso@mdstech.co.za>
2019-06-14 16:27:12 +0200

    Remove the cache class from the collivery class to separate logic

--- upload/system/library/mds/Collivery.php
+++ upload/system/library/mds/Collivery.php
@@ -636,7 +636,7 @@ class Collivery
      */
     private function getAddress($address_id)
     {
-        $cacheKey = 'address.' . $this->client_id . '.' .  ($address_id ?: '0');
+        $cacheKey = 'address.' . $this->client_id . '.' . ($address_id ?: '0');
         $address = $this->getCache($cacheKey);
         if ($address) {
             return $address;
@@ -991,6 +991,9 @@ class Collivery
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     private function enableCache()
     {
         $this->cacheEnabled = true;
@@ -1009,86 +1012,5 @@ class Collivery
     }
 }
 
-class Cache
-{
-    private $cache_dir;
-    private $cache;
 
-    public function __construct($cache_dir = null)
-    {
-        $this->cache_dir = $cache_dir;
-    }
-
-    public function has($name)
-    {
-        $cache = $this->load($name);
-
-        return is_array($cache) && ($cache['valid'] - 30) > time();
-    }
-
-    protected function load($name)
-    {
-        if (!isset($this->cache[$name])) {
-            if (file_exists($this->cache_dir . $name) && $content = file_get_contents($this->cache_dir . $name)) {
-                $this->cache[$name] = json_decode($content, true);
-
-                return $this->cache[$name];
-            }
-
-            $this->create_dir($this->cache_dir);
-        } else {
-            return $this->cache[$name];
-        }
-    }
-
-    protected function create_dir($dir_array)
-    {
-        /*if (!is_array($dir_array)) {
-            $dir_array = explode('/', $this->cache_dir);
-        }
-        array_pop($dir_array);
-        $dir = implode('/', $dir_array);
-
-        if ($dir != '' && !is_dir($dir)) {
-            $this->create_dir($dir_array);
-            if (!mkdir($dir) && !is_dir($dir)) {
-                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
-            }
-        }*/
-    }
-
-    public function get($name)
-    {
-        $cache = $this->load($name);
-        if (is_array($cache) && $cache['valid'] > time()) {
-            return $cache['value'];
-        }
-
-        return null;
-    }
-
-    public function put($name, $value, $time = 1440)
-    {
-        $cache = json_encode(['value' => $value, 'valid' => time() + ($time * 60)]);
-        if (file_put_contents($this->cache_dir . $name, $cache)) {
-            $this->cache[$name] = $cache;
-
-            return true;
-        }
-
-        return false;
-    }
-
-    public function forget($name)
-    {
-        $cache = json_encode(['value' => '', 'valid' => 0]);
-        if (file_put_contents($this->cache_dir . $name, $cache)) {
-            $this->cache[$name] = $cache;
-
-            return true;
-        }
-
-        return false;
-    }
-}
 
